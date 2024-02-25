1deb024c0e9f97bf7282938726bb4faf97d5dcca
zhtzm <zhouhaotian1314@outlook.com>
2024-02-25 16:22:18 +0800

    complete LeNet

---
+++ model/LeNet.py
@@ -0,0 +1,26 @@
+from torch import nn
+import torch.nn.functional as F
+
+
+class LeNet(nn.Module):
+    def __init__(self):
+        super(LeNet, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=5)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.fc1 = nn.Linear(32*5*5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, X):
+        X = F.relu(self.conv1(X))       # intput(b, 3, 32, 32) output(b, 16, 28, 28)
+        X = self.pool1(X)               # output(b, 16, 14, 14)
+        X = F.relu(self.conv2(X))       # output(b, 32, 10, 10)
+        X = self.pool2(X)               # output(b, 32, 5, 5)
+        X = X.view(-1, 32*5*5)          # output(b, 32*5*5)
+        X = F.relu(self.fc1(X))         # output(b, 120)
+        X = F.relu(self.fc2(X))         # output(b, 84)
+        X = self.fc3(X)                 # output(b, 10)
+
+        return X
---
+++ test/LeNet/train.py
@@ -0,0 +1,75 @@
+from model.LeNet import LeNet
+import torch
+import torchvision
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.transforms as transforms
+
+
+def main():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print("using {} device.".format(device))
+
+    # 定义一个transform用于处理图片
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    )
+
+    # 50000张训练图片
+    # 第一次使用时要将download设置为True才会自动去下载数据集
+    train_set = torchvision.datasets.CIFAR10(root='E:\Dataset', train=True,
+                                             download=False, transform=transform)
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=36,
+                                               shuffle=True, num_workers=0)
+    
+    # 10000张验证图片
+    # 第一次使用时要将download设置为True才会自动去下载数据集
+    val_set = torchvision.datasets.CIFAR10(root='E:\Dataset', train=False,
+                                           download=False, transform=transform)
+    val_loader = torch.utils.data.DataLoader(val_set, batch_size=5000,
+                                             shuffle=False, num_workers=0)
+    val_data_iter = iter(val_loader)
+    val_image, val_label = next(val_data_iter)
+    
+    net = LeNet().to(device=device)
+    loss_function = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(net.parameters(), lr=0.001)
+
+    for epoch in range(10):
+        running_loss = 0.
+        for step, data in enumerate(train_loader, start=0):
+            inputs, labels = data
+
+            if not inputs.device == device or not labels.device == device:
+                inputs = inputs.to(device=device)
+                labels = labels.to(device=device)
+
+            optimizer.zero_grad()
+            outputs = net(inputs)
+            loss = loss_function(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            if (step + 1) % 500 == 0:
+                with torch.no_grad():
+                    if not val_image.device == device or val_label.device == device:
+                        val_image = val_image.to(device=device)
+                        val_label = val_label.to(device=device)
+
+                    outputs = net(val_image)
+                    predict_y = torch.max(outputs, dim=1)[1]
+                    accuracy = torch.eq(predict_y, val_label).sum().item() / val_label.size(0)
+
+                    print('[%d, %5d] train_loss: %.3f  test_accuracy: %.3f' %
+                          (epoch + 1, step + 1, running_loss / 500, accuracy))
+                    running_loss = 0.0
+
+    print('Finished Training')
+
+    save_path = './save_model/Lenet.pth'
+    torch.save(net.state_dict(), save_path)
+
+if __name__ == '__main__':
+    main()
